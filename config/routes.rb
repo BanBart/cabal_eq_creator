18cd1db6f358bca49c72af7ee6ae95a7891a1281
Bartlomiej Banasik <bartlomiej.banasik@ananastar.com>
2015-09-27 20:15:48 +0200

    add subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+
+  get '/' => 'main#index', constraints: { subdomain: 'cabal' }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
