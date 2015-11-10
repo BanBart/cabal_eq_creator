19e785ffa505ba44626115a6a78b8f69252a7dfd
Bartlomiej Banasik <bartlomiej.banasik@ananastar.com>
2015-11-10 23:03:59 +0100

    add base page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+	root to: 'page#home'
 
-  scope module: :api, defaults: { format: 'json' }, constraints: { subdomain: 'api-cabal' } do 
-    scope module: :v1, constraints: ApiConstraint.new(version: 1, default: :true) do 
-      resources :weapons, only: [:index, :show]
-    end
-    match "*path", to: -> (env) { [404, {}, ['{"errors": [ {"message": "method not found", "status": "404"} ] }']] }, via: :all
-  end
+  # scope module: :api, defaults: { format: 'json' }, constraints: { subdomain: 'api-cabal' } do 
+  #   scope module: :v1, constraints: ApiConstraint.new(version: 1, default: :true) do 
+  #     resources :weapons, only: [:index, :show]
+  #   end
+  #   match "*path", to: -> (env) { [404, {}, ['{"errors": [ {"message": "method not found", "status": "404"} ] }']] }, via: :all
+  # end
 end
